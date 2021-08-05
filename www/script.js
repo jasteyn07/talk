5a48f205b8aa17719a89f2596d184baf3e082e5e
Jan Steyn <jasteyn07@gmail.com>
2021-08-05 14:08:51 +0200

    Update script.js

--- www/script.js
+++ www/script.js
@@ -1,22 +1,9 @@
 /* globals App, io, cabin*/
 const ICE_SERVERS = [
 	{ urls: "stun:stun.l.google.com:19302" },
-	{ urls: "stun:stun.stunprotocol.org:3478" },
-	{ urls: "stun:stun.sipnet.net:3478" },
-	{ urls: "stun:stun.ideasip.com:3478" },
-	{ urls: "stun:stun.iptel.org:3478" },
 	{ urls: "stun3.l.google.com:19302" },
 	{ urls: "turn:numb.viagenie.ca", username: "jan@goldkeytechnologies.com", credential: "SJP67sURDsnm!S9" },
-	{
-		urls: [
-			"turn:173.194.72.127:19305?transport=udp",
-			"turn:[2404:6800:4008:C01::7F]:19305?transport=udp",
-			"turn:173.194.72.127:443?transport=tcp",
-			"turn:[2404:6800:4008:C01::7F]:443?transport=tcp",
-		],
-		username: "CKjCuLwFEgahxNRjuTAYzc/s6OMT",
-		credential: "u1SQDR/SQsPQIxXNWQT7czc/G4c=",
-	},
+	{ urls: "stun:numb.viagenie.ca", username: "jan@goldkeytechnologies.com", credential: "SJP67sURDsnm!S9" },
 ];
 
 const APP_URL = (() => {
