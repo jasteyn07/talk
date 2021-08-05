c19e93fef72f7f5d8a46cd1b5768e6c1c687f598
Jan Steyn <jasteyn07@gmail.com>
2021-08-05 14:02:05 +0200

    Update script.js

--- www/script.js
+++ www/script.js
@@ -6,7 +6,7 @@ const ICE_SERVERS = [
 	{ urls: "stun:stun.ideasip.com:3478" },
 	{ urls: "stun:stun.iptel.org:3478" },
 	{ urls: "stun3.l.google.com:19302" },
-	{ urls: "turn:numb.viagenie.ca", username: "imvasanthv@gmail.com", credential: "d0ntuseme" },
+	{ urls: "turn:numb.viagenie.ca", username: "jan@goldkeytechnologies.com", credential: "SJP67sURDsnm!S9" },
 	{
 		urls: [
 			"turn:173.194.72.127:19305?transport=udp",
